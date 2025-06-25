b51a15f47a045f6a83c5250ded4e33aff79fbe95
Venkat G R <grvenkat456@gmail.com>
2025-06-25 20:01:40 +0530

    Initial commit with flast app structure

---
+++ README.md
@@ -0,0 +1,42 @@
+# 🧬 HematoVision: Advanced Blood Cell Classification Using Transfer Learning
+
+HematoVision is a deep learning-based web application that classifies various types of blood cells using a pre-trained VGG16 model and Flask. It is designed to assist pathologists, medical professionals, and learners by enabling accurate and real-time classification of blood cell images.
+
+---
+
+## 📌 Features
+
+- ✅ Classifies blood cells into 5 categories
+- 🧠 Built using **Transfer Learning (VGG16)**
+- 🌐 Interactive web interface with **Flask**
+- 📂 Upload your own blood cell images for prediction
+- 🧪 Preloaded sample images to download and test
+- 📉 Real-time prediction with confidence scores
+
+---
+
+## 🧪 Categories Covered
+
+- **NEUTROPHIL**
+- **EOSINOPHIL**
+- **MONOCYTE**
+- **LYMPHOCYTE**
+- **BASOPHIL**
+
+---
+
+## 🔧 Technologies Used
+
+- Python
+- TensorFlow / Keras
+- Flask
+- OpenCV
+- HTML + CSS
+---
+## 🔗 Download Pretrained Model
+
+This project requires a trained CNN model file to make predictions.
+
+👉 [Download Blood_Cell_PRED.h5](https://drive.google.com/file/d/12Bbh3kaEBFsE2WLr3ymufSdu4bfi1WUr/view?usp=drive_link)
+
+Place the file in the root directory of the project (same location as `app.py`).
---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask,render_template,redirect,url_for
+import numpy as np
+import cv2
+import os
+from flask import request
+from tensorflow.keras.models import load_model
+from werkzeug.utils import secure_filename
+
+app=Flask(__name__)
+upload_folder='static/uploads'
+app.config['upload_folder']=upload_folder
+
+model=load_model('Blood_Cell_PRED.h5')
+class_names=['BASOPHIL', 'EOSINOPHIL', 'LYMPHOCYTE', 'MONOCYTE', 'NEUTROPHIL']
+
+def predict_image(img_path):
+    img=cv2.imread(img_path)
+    img=cv2.resize(img,(224,224))
+    img=img/255.0
+    img=np.expand_dims(img,axis=0)
+    prediction=model.predict(img)[0]
+    class_idx=np.argmax(prediction)
+    label=class_names[class_idx]
+    confidence=prediction[class_idx]
+    return label,confidence
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+
+def predict():
+    if 'file' not in request.files:
+        return 'No File uploaded'
+    file=request.files['file']
+    if file.filename == '':
+        return 'Empty Filename'
+    filename=secure_filename(file.filename)
+    file_path=os.path.join(app.config['upload_folder'],filename)
+    file.save(file_path)
+
+    label,confidence=predict_image(file_path)
+    return render_template('result.html',label=label,confidence=confidence,filename=filename)
+
+if __name__=='__main__':
+    app.run(debug=True)
